5b59806f28daabb43faa517df0b4f5284f89f153
Santosh Lamichhane <ssrusan@gmail.com>
2023-06-21 20:33:51 +0545

    thapa10

---
+++ thapa10.js
@@ -0,0 +1,55 @@
+// let arr=[3,5,7]
+//[3,10,7]
+// for(let i=0;i<arr.length;i++){
+  
+//   if(arr[i]%5==0){
+//     arr[i]=arr[i]+5;
+//   }
+// }
+// arr
+
+
+
+// const arr=[1,2,3,4]
+// const myArr=arr
+// myArr.push(100)
+// console.log(arr)
+// console.log(myArr)
+// myArr=[...arr]
+// myArr.push(100)
+// myArr
+// arr
+// arr.toString()
+
+
+// const obj={
+//   name:'ram',
+//     roll:3,
+//       age:32
+// }
+
+// const myObj=obj
+// myObj.address='banepa'
+// obj
+
+// myObj={...obj,address:'banepa'}
+
+// const myObj=JSON.stringfy(myObj)
+// console.log(myObj)
+
+
+// const obj = {name: "John", age: 30, city: "New York"};
+// const myJSON = JSON.stringify(obj);
+// console.log(myJSON)
+
+
+const obj={
+    name:'ram',
+      roll:3,
+    batch:4,
+        age:32
+  }
+  const {name,age}=obj
+  
+  
+  
